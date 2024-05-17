1a321de83d84a8c8fb3455aa7877c011fc48dc7a
Kanan Farzali <kanan.farzali@gmail.com>
2024-05-17 13:48:29 +0200

    server forms with useFormState hook

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,3 +19,47 @@ export const deleteSnippet = async (id: number) => {
 
   redirect('/');
 };
+
+export const createSnippet = async (
+  formState: { message: string },
+  formData: FormData
+) => {
+  try {
+    // Check the user's inputs and make sure they are valid
+    const title = formData.get('title');
+    const code = formData.get('code');
+
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer',
+      };
+    }
+
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be longer',
+      };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: 'Something went wrong...',
+      };
+    }
+  }
+
+  // Re-direct the user back to the root route
+  redirect('/');
+};
